2faa5bd6394076bd678cdf97c8aa70c60b2c5271
SoubirSaian <soubir2018@gmail.com>
2021-09-26 20:10:30 +0530

    semi-final-draft

--- yash-raj-films/src/component/Person/Person.js
+++ yash-raj-films/src/component/Person/Person.js
@@ -8,12 +8,26 @@ const Person = (props) => {
         <div>
             <div className="display-person">
                 <img className="person-img" src={img} alt="" />
+
+                 
+
                 <h2>{name}</h2>
+                 
                 <h1>{role}</h1>
+                
                 <h4>Age : {age}</h4>
+                
                 <h3>Country : {country}</h3>
+                
                 <h3>Fee : {salary} Cr.</h3>
+
+                {/* sociaal mmedia icon  */}
+                <span><i class="fab fa-facebook-square media-icon"></i><i class="fab fa-twitter-square media-icon"></i></span>
+                   {/* regular button   */}
+                   
                 <button className="btn-regular" onClick={()=> props.buttonClick(props.name)}>select</button>
+
+                
             </div>
              
         </div>
